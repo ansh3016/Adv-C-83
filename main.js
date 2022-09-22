ea6bb6bc744373697209dab26624784c2972ea84
ansh3016 <96235951+ansh3016@users.noreply.github.com>
2022-09-22 16:53:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ console.log("x = "+ last_position_of_x +"y="+ last_position_of_y);
 ctx.moveTo(last_position_of_x, last_position_of_y);
 
 console.log("Current position of x and y coordinates =");
-console.log("x = "+ current_position_of_x +"y="+ current_position_of_y);
+console.log("x = "+ current_position_of_touch_x +"y="+ current_position_of_touch_y);
 ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
 ctx.stroke();
 
